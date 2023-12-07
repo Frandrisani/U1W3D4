3a447bc405094f59bbcc541f2d11411714938616
Frandrisani <fra.andrisani01@gmail.com>
2023-12-07 21:13:03 +0100

    Modifiche al codice js e css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const tombola = function () {
+  const spazioNumeriJs = document.getElementById("spazioNumeri");
+  const numeriEstratti = [];
+  //Inizio creiazione di 90 celle
   for (let i = 0; i < 90; i++) {
-    const spazioNumeriJs = document.getElementById("spazioNumeri");
     const cellaNumero = document.createElement("div");
     cellaNumero.classList.add("cellaNumeroStyle");
     const cellValue = document.createElement("h3");
@@ -8,15 +10,26 @@ const tombola = function () {
     cellaNumero.appendChild(cellValue);
     spazioNumeriJs.appendChild(cellaNumero);
   }
+  //fine creazione di 90 celle
 
   //creo una funzione per generare numeri da 1 a 90 e stamparli nel documento
   const generatoreDiNumeri = function () {
-    const numeroGenerato = Math.floor(Math.random() * 90 + 1);
-    const mostraNumeroGeneratoInHtml = document.createElement("p");
-    mostraNumeroGeneratoInHtml.innerHTML = `Il numero generato è: ${numeroGenerato}`;
-    document
-      .getElementById("spazioGeneraNumero")
-      .appendChild(mostraNumeroGeneratoInHtml);
+    let numeroGenerato;
+
+    do {
+      numeroGenerato = Math.floor(Math.random() * 90) + 1;
+    } while (numeriEstratti.includes(numeroGenerato));
+    numeriEstratti.push(numeroGenerato);
+
+    document.getElementById("numerogenerato").innerText = numeroGenerato;
+
+    let celleNumeri = document.querySelectorAll(".cellaNumeroStyle h3");
+
+    celleNumeri.forEach(function (cell) {
+      if (parseInt(cell.innerText) === numeroGenerato) {
+        cell.parentNode.classList.add("estratta");
+      }
+    });
   };
 
   //ora generiamo i numeri con il pulsante; ogni qual volta si clicca il pulsante si genera un numero casuale richiamando la funzione generatoreDiNumeri()
@@ -24,15 +37,6 @@ const tombola = function () {
   bottone.addEventListener("click", function () {
     generatoreDiNumeri();
   });
-
-  //   // evidenziamo il giorno selezionato nel calendario per mostrare che ha almeno un evento!
-  //   const dot = document.createElement('span')
-  //   dot.classList.add('dot')
-  //   // trovo la cella "day" selected
-  //   // const selectedCell = document.querySelector('.selected') <-- RIP orsi polari
-  //   const selectedCell = document.getElementsByClassName('selected')[0]
-  //   selectedCell.appendChild(dot)
-  // }
 };
 
 tombola();
